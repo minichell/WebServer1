ee282636e7e1d8f3a6feb9c2994b3c575a360cb5
minichell <54796879+minichell@users.noreply.github.com>
2020-09-29 11:42:14 +0200

    klar!

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const clientDir = __dirname + "\\client\\"
+
+//MONGODB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', function() {
+    //
+});
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+
+personSchema.methods.speak = () => {
+    console.log("cool");
+    conssole.log("The name is " + this.name);
+
+}
+
+const Person = mongoose.model('person', personSchema);
+// END OF MONGODB
+
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -20,7 +45,12 @@ app.get('/theBaddest', (req, res) => {
 app.post('/', (req, res) => {
    console.log(req.body.name) 
    console.log(req.body.email)
+   console.log(req.body.age)
+   const person = new Person ({name: req.body.name, email: req.body.email, age: req.body.age});
+   person.save()
    res.redirect('/')
 })
+
+
 
 app.listen(port,() => console.log(`Example app listening on port port!`))
--- client/mongoTest.js
+++ client/mongoTest.js
@@ -1,15 +1,14 @@
 const mongoose = require ('mongoose');
-
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-
 });
 
 const kittySchema = new mongoose.Schema({
-name: String
+name: String,
+email: String
 
 });
 
